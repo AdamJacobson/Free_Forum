938b87c80a9bb54b04158945927f142143d2af71
Adam <amartenj@gmail.com>
2014-08-01 14:43:02 -0700

    Users sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 FreeForum::Application.routes.draw do
 	root  'static_pages#home'
-	match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
 
   resources :users
+	match '/signup',  to: 'users#new',            via: 'get'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+ 	match '/signin',  to: 'sessions#new',         via: 'get'
+	match '/signout', to: 'sessions#destroy',     via: 'delete'
   
 end

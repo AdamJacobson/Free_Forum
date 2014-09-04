cfaaab23fcce61252cccc185ce3940161310e780
Adam <amartenj@gmail.com>
2014-09-04 15:33:43 -0700

    Admin can create board. Posts, topics and boards alternate background color

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,6 +11,29 @@ class BoardsController < ApplicationController
 	end
 
 	def index
-		@boards = Board.paginate(page: params[:page])
+		@boards = Board.all
 	end
+
+	def new
+		@board = Board.new
+
+		add_breadcrumb "Create New Board"
+	end
+
+	def create
+		@board = Board.create(board_params)
+
+		if @board.save
+			flash[:success] = "Board created"
+			redirect_to @board
+		else
+			render 'new'
+		end
+	end
+
+		private
+
+		def board_params
+			params.require(:board).permit(:title, :description)
+		end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,4 @@
 class Board < ActiveRecord::Base
 	has_many :topics
 	has_many :posts, :through => :topics
-
-	self.per_page = 10
 end

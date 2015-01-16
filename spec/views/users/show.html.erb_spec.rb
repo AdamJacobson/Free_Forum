2e91e6b45242ee656271dcaff9cd78ca0c0a6c9c
Adam <amartenj@gmail.com>
2015-01-15 20:10:57 -0800

    Commit before code base move

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -7,7 +7,7 @@ describe "users/show.html.erb" do
 
 	subject { page }
 
-	describe "by default" do
+	describe "of a new user" do
 		before { visit user_path(user) }
 
 		it { should have_title(full_title("Profile for #{user.username}")) }
@@ -18,11 +18,38 @@ describe "users/show.html.erb" do
 	end
 
 	describe "for an admin" do
-		pending
+		let!(:admin) { create(:admin) }
+		before do
+			sign_in(admin)
+			visit user_path(user)
+		end
+
+		it { should have_content("User Controls") }
+		it { should have_content("Moderating Boards") }
 	end
 
 	describe "for a moderator" do
-		pending
+		let!(:moderator) { create(:user) }
+		before do
+			ModeratorJoin.create(board: board, user: moderator)
+			sign_in(moderator)
+			visit user_path(user)
+		end
+
+		it { should have_content("Grant user moderating privilages") }
+		it { should have_selector("option") }
+		it { should_not have_content("User Controls") }
+		it { should_not have_content("Moderating Boards") }
+
+		describe "after granting moderator privilages" do
+			before do
+				ModeratorJoin.create(board: board, user: user)
+				visit user_path(user)
+			end
+
+			it { should have_content("You cannot grant this user any more moderating privilages") }
+			it { should_not have_selector("option") }
+		end
 	end
 
 	describe "with at least 1 post" do
